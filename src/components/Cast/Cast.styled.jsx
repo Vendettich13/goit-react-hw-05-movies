5149731e31f8b5d36517ffa4fbc0c0f7e9086ff9
Victor Litvynchuk <vendettich13@icloud.com>
2022-12-25 01:05:13 +0200

    Update Cast.styled.jsx

--- src/components/Cast/Cast.styled.jsx
+++ src/components/Cast/Cast.styled.jsx
@@ -5,7 +5,7 @@ export const List = styled.ul`
     text-decoration: none;
     display: grid;
     grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: repeat(4, 1fr);
+    grid-template-rows: repeat(1, 1fr);
     grid-gap: 20px;
     border-bottom: 3px solid;
     border-image: linear-gradient(45deg, rgb(30, 1, 252), rgb(255, 255, 12)) 1;
